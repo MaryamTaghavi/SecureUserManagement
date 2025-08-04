6b25e9ef6adbaadc2bdaa392ea4dd9730c52f719
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-04 13:55:51 +0330

    generateSecretKey

--- SecureUserManagement/SecureUserManagement/Services/AuthService.cs
+++ SecureUserManagement/SecureUserManagement/Services/AuthService.cs
@@ -3,19 +3,19 @@ using Microsoft.IdentityModel.Tokens;
 using SecureUserManagement.Data;
 using SecureUserManagement.Infrustructure;
 using SecureUserManagement.Interfaces;
+using SecureUserManagement.Model;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using SecureUserManagement.Model;
 
-namespace SecureUserManagement.Services; 
+namespace SecureUserManagement.Services;
 
 public class AuthService : IAuthService
 {
     private readonly AppDbContext _context;
     private readonly IConfiguration _configuration;
 
-    public AuthService(AppDbContext context , IConfiguration configuration)
+    public AuthService(AppDbContext context, IConfiguration configuration)
     {
         _context = context;
         _configuration = configuration;
@@ -23,7 +23,7 @@ public class AuthService : IAuthService
 
     public async Task<string> LoginWithPasswordAsync(LoginRequest login, CancellationToken cancellationToken)
     {
-        var user = await _context.Users.FirstOrDefaultAsync(r => r.Name == login.UserName && r.Password == login.Password , cancellationToken);
+        var user = await _context.Users.FirstOrDefaultAsync(r => r.Name == login.UserName && r.Password == login.Password, cancellationToken);
 
         if (user == null)
         {
@@ -41,7 +41,7 @@ public class AuthService : IAuthService
             new Claim(ClaimTypes.Name, user.Name),
         };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:SecretKey"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
         var token = new JwtSecurityToken(
